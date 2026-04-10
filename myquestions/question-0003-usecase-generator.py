5c5b51cc02bc7cda69ba30ee85c54e753e0593da
mariangelurzola-cmyk <mariangel.urzola@udea.edu.co>
2026-04-10 11:10:22 -0500

    Update question-0003-usecase-generator.py

--- myquestions/question-0003-usecase-generator.py
+++ myquestions/question-0003-usecase-generator.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+def generar_caso_de_uso():
+    # Generar 49 datos normales y 1 dato final (posible anomalía)
+    base = np.random.normal(100, 5, 49)
 def detectar_anomalia_presion(serie_datos):
     pass
 
